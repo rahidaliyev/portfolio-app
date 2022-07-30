ba76562ac19cf275458c63ef4bcaffa6a80b9799
Rahid Aliyev <rahidaliyev18@gmail.com>
2022-07-30 12:08:33 +0400

    style added to loader

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import {Route,Routes} from 'react-router-dom'
 import DefaultPage from './components/DefaultPage';
 import HireMe from './components/HireMe/HireMe';
 import RingLoader from "react-spinners/RingLoader";
-import { AbsoluteCenter } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 
 function App() {
+  const style={
+    borderColor: "red",
+    position:'absolute',
+    left:'45%',
+    top:'245px'
+
+  }
   const [loading,setLoading]=useState(false)
   useEffect(()=>{
       setLoading(true)
       setTimeout(()=>{
           setLoading(false)
-      },8000)
+      },3000)
   },[])
   return (
 <div>
 {
    loading ?
+   <div>
    <RingLoader
+color="blue"
 size={150}
 loading={loading}
-style={{color:'blue.700',position:'absolute',left:'45%',top:'245px'}}
+style={style}
 />
+{/* <Box style={style}>Lütfən gözləyin....</Box> */}
+</div>
 :
 <div>
 <Routes>
